355f9f71f41faff66c2d753e8ebc5f315d62ce18
sergio <sergio.rios@payco.co>
2025-01-23 19:55:49 -0500

    conexion a microservicio de productos

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,15 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  PRODUCTS_MICROSERVICES_HOST: string;
+  PRODUCTS_MICROSERVICES_PORT: number;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -21,4 +25,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  productsMicroservicesHost: envVars.PRODUCTS_MICROSERVICES_HOST,
+  productsMicroservicesPort: envVars.PRODUCTS_MICROSERVICES_PORT,
 };
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { PRODUCT_SERVICE } from '../config';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from '../common';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct() {
+    return 'Create product';
+  }
+
+  @Get()
+  findProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findOneProduct(@Param('id') id: string): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+      );
+      return response;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return 'Delete product' + id;
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body() body: any) {
+    return 'Update product' + id;
+  }
 }
